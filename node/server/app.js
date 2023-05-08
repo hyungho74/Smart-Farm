80fd03fa1f8cec94d6a9d48a04b2a5e90034ba48
hyungho74 <khh050704@gmail.com>
2023-05-08 21:00:28 +0900

    node.js server ~05.08

---
+++ node/server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const port = 8070;
+const server = app.listen(port, function() {
+    console.log('Listening on '+port);
+});
+
+const SocketIO = require('socket.io');
+const io = SocketIO(server, {path: '/socket.io'});
+
+io.on('connection', function (socket) {
+    console.log(socket.id, ' connected...');
+    
+    // broadcasting a entering message to everyone who is in the chatroom
+    io.emit('msg', `${socket.id} has entered the chatroom.`);
+    io.on('msg', function (data){
+        console.log(socket.id, ':', data);
+        socket.broadcast.emit('msg', `${socket.id}: ${data}`);
+    });
+  	// message receives
+    socket.on('msg', function (data) {
+        console.log(socket.id,': ', data);
+        // broadcasting a message to everyone except for the sender
+        socket.broadcast.emit('msg', `${socket.id}: ${data}`);
+    });
+
+    // user connection lost
+    socket.on('disconnect', function (data) {
+        io.emit('msg', `${socket.id} has left the chatroom.`);
+    });
+});
+
+app.get('/chat', function(req, res) {
+    res.sendFile(__dirname + '/chat.html');
+});
