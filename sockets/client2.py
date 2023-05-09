e5359f3d1b1af08dab469bec79f7403edd7e401b
hyungho74 <khh050704@gmail.com>
2023-05-09 20:38:59 +0900

    smart-farm client

--- sockets/client2.py
+++ sockets/client2.py
@@ -1,32 +1,60 @@
-import socket
-from threading import Thread
+import time
+import requests
+import socketio
 
-HOST = '10.82.17.194'
-PORT = 9009
 
-def rcvMsg(sock):
-   while True:
-      try:
-         data = sock.recv(1024)
-         if not data:
-            break
-         print(data.decode())
-      except:
-         pass
+# Socket.IO 클라이언트 생성
+sio = socketio.Client()
 
-def runChat():
-   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-      sock.connect((HOST, PORT))
-      t = Thread(target=rcvMsg, args=(sock,))
-      t.daemon = True
-      t.start()
+# Socket.IO 이벤트 핸들러
+@sio.event
+def connect():
+    print('Socket.IO 서버에 연결되었습니다.')
 
-      while True:
-         msg = input()
-         if msg == '/quit':
-            sock.send(msg.encode())
-            break
+@sio.event
+def disconnect():
+    print('Socket.IO 서버와 연결이 끊어졌습니다.')
 
-         sock.send(msg.encode())
-            
-runChat()
+# 데이터 전송 함수
+def send_sensor_data(data):
+    # Socket.IO 서버로 데이터 전송
+    sio.emit('sensor_data', data)
+    print('센서 데이터 전송:', data)
+
+# 측정 함수
+def read_sensor_values():
+    # 온습도 센서(DHT11) 읽기
+    temperature = 23
+    humidity = 18
+    light = 100
+
+    # 수위 센서 읽기
+    water_level = 90
+
+    # 토양 습도 센서 읽기
+    soil_moisture = 70
+
+    # 센서 데이터 반환
+    sensor_data = {
+        'temperature': temperature,
+        'humidity': humidity,
+        'light': light,
+        'water_level': water_level,
+        'soil_moisture': soil_moisture
+    }
+    return sensor_data
+
+# Socket.IO 서버에 연결
+sio.connect('http://localhost:3000')
+
+# 주기적으로 센서 값을 읽고 전송
+while True:
+    try:
+        sensor_data = read_sensor_values()
+        send_sensor_data(sensor_data)
+        time.sleep(10)  # 10초마다 측정 및 전송
+    except KeyboardInterrupt:
+        break
+
+# Socket.IO 클라이언트 연결 종료
+sio.disconnect()
