39c40916b52ce1814d25aa09be42d3653ccf7809
hyungho74 <khh050704@gmail.com>
2023-05-09 20:38:43 +0900

    chat server client

--- sockets/client.py
+++ sockets/client.py
@@ -1,22 +1,30 @@
 import socketio
-import time
 
+# Socket.IO 클라이언트 생성
 sio = socketio.Client()
+
+# Socket.IO 이벤트 핸들러
 @sio.event
 def connect():
-    print("connect")
+    print('Socket.IO 서버에 연결되었습니다.')
 
-@sio.event
-def connect_error():
-    print("connect_error")
-    
 @sio.event
 def disconnect():
-    print("disconnect")
-
-sio.connect('http://localhost:8070')
-print("my sid is",sio.sid)
+    print('Socket.IO 서버와 연결이 끊어졌습니다.')
 
 @sio.event
-def msg(data):
-    print(data)
+def chat_message(data):
+    print('새로운 메시지:', data)
+
+# Socket.IO 서버에 연결
+sio.connect('http://localhost:8070')
+
+# 채팅 입력 및 전송
+while True:
+    message = input('메시지를 입력하세요 (나가기: q): ')
+    if message == 'q':
+        break
+    sio.emit('chat_message', message)
+
+# Socket.IO 클라이언트 연결 종료
+sio.disconnect()
