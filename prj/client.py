af85266a0f4811c75d6f898f73f752c5dba29c03
hyungho74 <khh050704@gmail.com>
2023-05-11 20:10:28 +0900

    smart-farm client(-완-)

---
+++ prj/client.py
@@ -0,0 +1,66 @@
+import time
+import requests
+import socketio
+from water import watersensor
+from pump import waterpump,sand
+from fan import dht, fan
+from light import lightsensor, led
+
+
+
+# Socket.IO 클라이언트 생성
+sio = socketio.Client()
+
+# Socket.IO 이벤트 핸들러
+@sio.event
+def connect():
+    print('Socket.IO 서버에 연결되었습니다.')
+
+@sio.event
+def disconnect():
+    print('Socket.IO 서버와 연결이 끊어졌습니다.')
+
+# 데이터 전송 함수
+def send_sensor_data(data):
+    # Socket.IO 서버로 데이터 전송
+    sio.emit('sensor_data', data)
+    print('센서 데이터 전송:', data)
+
+# 측정 함수
+def read_sensor_values():
+    # 온습도 센서(DHT11) 읽기
+    temperature ,humidity = dht()
+    light = lightsensor()
+
+    # 수위 센서 읽기
+    water_level = watersensor()
+
+    # 토양 습도 센서 읽기
+    soil_moisture = sand()
+    waterpump(30)
+    fan(20)
+    led()
+    # 센서 데이터 반환
+    sensor_data = {
+        'temperature': temperature,
+        'humidity': humidity,
+        'light': light,
+        'water_level': water_level,
+        'soil_moisture': soil_moisture
+    }
+    return sensor_data
+
+# Socket.IO 서버에 연결
+sio.connect('http://10.82.17.194:3000')
+
+# 주기적으로 센서 값을 읽고 전송
+while True:
+    try:
+        sensor_data = read_sensor_values()
+        send_sensor_data(sensor_data)
+        time.sleep(10)  # 10초마다 측정 및 전송
+    except KeyboardInterrupt:
+        break
+
+# Socket.IO 클라이언트 연결 종료
+sio.disconnect()
